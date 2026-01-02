5409662ec8885eb65f5d4acd6166c1f090f07115
Chaim Halbert <chaim.leib.halbert@gmail.com>
2026-01-01 18:21:40 -0700

    feat(test): add TestLogger to capture logs during test

---
+++ test/log.go
@@ -0,0 +1,26 @@
+package test
+
+import (
+	"bytes"
+	"fmt"
+	"log"
+	"testing"
+)
+
+// TestLogger captures the output send to the log package.
+// If the test fails, the logs are printed.
+// Otherwise, they are suppressed.
+//
+// It returns the buffer in case the logged output needs to be checked.
+func TestLogger(t *testing.T) fmt.Stringer {
+	var buf bytes.Buffer
+	log.SetFlags(0) // suppress timestamps
+	log.SetOutput(&buf)
+	t.Cleanup(func() {
+		if t.Failed() {
+			t.Log("log output:")
+			t.Log(buf.String())
+		}
+	})
+	return &buf
+}
