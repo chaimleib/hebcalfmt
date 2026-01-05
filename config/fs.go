8685530550aec1cc47ef04c33f8f970a8f190fd4
Chaim Halbert <chaim.leib.halbert@gmail.com>
2026-01-05 16:29:25 -0700

    feat(config): add WrapFS type and tests

--- config/fs.go
+++ config/fs.go
@@ -1,8 +1,11 @@
 package config
 
 import (
+	"fmt"
 	"io/fs"
 	"os"
+	"path"
+	"path/filepath"
 )
 
 // DefaultFS returns a new [fs.FS] for use in loading configuration files.
@@ -25,4 +28,24 @@ func (fsf FSFunc) Open(fpath string) (fs.File, error) {
 // into an [fs.FS].
 func NewFSFunc[F fs.File](fn func(string) (F, error)) fs.FS {
 	return FSFunc(func(fpath string) (fs.File, error) { return fn(fpath) })
+}
+
+type WrapFS struct {
+	BaseDir string
+	FS      fs.FS
+}
+
+func (w WrapFS) Open(fpath string) (fs.File, error) {
+	if !filepath.IsLocal(fpath) {
+		return nil, fmt.Errorf(
+			"attempted access outside of the config file's directory tree: open %s",
+			fpath,
+		)
+	}
+	// use path instead of filepath, which is os-sensitive.
+	// os.Open will work on Windows
+	// with forward slashes instead of its preferred backslashes;
+	// but internet-based FSes and so on are likely to break if given backslashes.
+	resolved := path.Join(w.BaseDir, fpath)
+	return w.FS.Open(resolved)
 }
--- config/fs_test.go
+++ config/fs_test.go
@@ -6,6 +6,7 @@ import (
 	"testing/fstest"
 
 	"github.com/chaimleib/hebcalfmt/config"
+	"github.com/chaimleib/hebcalfmt/test"
 )
 
 func TestLocalFS(t *testing.T) {
@@ -55,5 +56,51 @@ func TestFSFunc(t *testing.T) {
 
 	if string(buf) != want {
 		t.Errorf("want: %q, got: %q", want, string(buf))
+	}
+}
+
+func TestWrapFS(t *testing.T) {
+	const want = "hi, I'm Sub!"
+	testFS := fstest.MapFS{
+		"hi.txt":     &fstest.MapFile{Data: []byte("hi, I'm Root!")},
+		"sub/hi.txt": &fstest.MapFile{Data: []byte(want)},
+	}
+	cases := []struct {
+		Name    string
+		Sub     string
+		Fpath   string
+		Want    string
+		OpenErr string
+	}{
+		{Name: "sub", Sub: "sub", Fpath: "hi.txt", Want: "hi, I'm Sub!"},
+		{
+			Name:    "double dots",
+			Sub:     "sub",
+			Fpath:   "../sub/hi.txt",
+			OpenErr: "attempted access outside of the config file's directory tree: open ../sub/hi.txt",
+		},
+	}
+	for _, c := range cases {
+		t.Run(c.Name, func(t *testing.T) {
+			wfs := config.WrapFS{BaseDir: c.Sub, FS: testFS}
+
+			f, err := wfs.Open(c.Fpath)
+			test.CheckErr(t, err, c.OpenErr)
+			if err != nil {
+				return
+			}
+			defer f.Close()
+
+			buf, err := io.ReadAll(f)
+			if err != nil {
+				t.Errorf("unexpected error while reading %s from %s: %v",
+					c.Fpath, c.Sub, err)
+			}
+
+			if want != string(buf) {
+				t.Errorf("contents of %s from %s did not match - want:\n%s\ngot:\n%s",
+					c.Fpath, c.Sub, want, string(buf))
+			}
+		})
 	}
 }
